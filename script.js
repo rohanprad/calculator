2e82ea5738a1bfc846cf274a85da7b293dd768e1
Rohan Pradhan <rohanprad09@gmail.com>
2020-09-19 15:19:55 +0530

    imrpoved functionality

--- script.js
+++ script.js
@@ -1,227 +1,89 @@
-function add(num1, num2){
-    return num1 + num2;
-}
+let displayText = "";
+let number1 = null;
+let number2 = null;
+let operator = "";
+let carriedOperator = "";
 
-function subtract(num1, num2){
-    return num1 - num2;
-}
-
-function multiply(num1, num2){
-    return num1 * num2;
-}
-
-function divide(num1, num2){
-    return num1/num2;
-}
-
-function operate(num1, num2 ,operator){
-    switch(operator){
-
-        case '+':
-        return add(num1, num2);
-        break;
-
-        case '-':
-        return subtract(num1,num2);
-        break;
-
-        case '*':
-        return multiply(num1,num2);
-        break;
-
-        case '/':
-        return divide(num1, num2);
-        break;
-
-        default:
-        return "error";
-        break;
+function operate(a, b, operator){
+    if(operator === "add"){
+        return a+b;
+    }else if(operator === "subtract"){
+        return a-b;
+    }else if(operator === "multiply"){
+        return a*b;
+    }else if(operator === "divide"){
+        return a/b;
     }
 }
 
-let displayValue = "";
-let num1;
-let num2;
-let operator;
+const numbers = document.querySelectorAll('.numbers');
+const operators = document.querySelectorAll('.operators');
+const displayDiv = document.querySelector('.display');
+const clear = document.querySelector('.clear');
 
-const display = document.querySelector('p');
+function handleNumClick(e){
+    displayText += e.target.classList[1];
+    displayDiv.innerHTML = `<p>${displayText}</p>`
+}
 
-
-const nine = document.querySelector('#nine');
-nine.addEventListener('click',function(){
-    displayValue += "9";
-    display.textContent = displayValue;
-    if(operator!= "")
-        num2 = 9;
-    else
-        num1 = 9;
-})
-
-const eight = document.querySelector('#eight');
-eight.addEventListener('click',function(){
-    displayValue += "8";
-    display.textContent = displayValue;
-    if(operator!= "")
-        num2 = 8;
-    else
-        num1 = 8;
-})
-
-const seven = document.querySelector('#seven');
-seven.addEventListener('click',function(){
-    displayValue += "7";
-    display.textContent = displayValue;
-    if(operator!= "")
-        num2 = 7;
-    else
-        num1 = 7;
-})
-
-const six = document.querySelector('#six');
-six.addEventListener('click',function(){
-    displayValue += "6";
-    display.textContent = displayValue;
-    if(operator!= "")
-        num2 = 6;
-    else
-        num1 = 6;
-})
-
-const five = document.querySelector('#five');
-five.addEventListener('click',function(){
-    displayValue += "5";
-    display.textContent = displayValue;
-    if(operator!= "")
-        num2 = 5;
-    else
-        num1 = 5;
-})
-
-const four = document.querySelector('#four');
-four.addEventListener('click',function(){
-    displayValue += "4";
-    display.textContent = displayValue;
-    if(operator!= "")
-        num2 = 4;
-    else
-        num1 = 4;
-})
-
-const three = document.querySelector('#three');
-three.addEventListener('click',function(){
-    displayValue += "3";
-    display.textContent = displayValue;
-    if(operator!= "")
-        num2 = 3;
-    else
-        num1 = 3;
-})
-
-const two = document.querySelector('#two');
-two.addEventListener('click',function(){
-    displayValue += "2";
-    display.textContent = displayValue;
-    if(operator!= "")
-        num2 = 2;
-    else
-        num1 = 2;
-})
-
-const one = document.querySelector('#one');
-one.addEventListener('click',function(){
-    displayValue += "1";
-    display.textContent = displayValue;
-    if(operator!= "")
-        num2 = 1;
-    else
-        num1 = 1;
-})
-
-const zero = document.querySelector('#zero');
-zero.addEventListener('click',function(){
-    displayValue += "0";
-    display.textContent = displayValue;
-    if(operator!= "")
-        num2 = 0;
-    else
-        num1 = 0;
-})
-
-const clear = document.querySelector("#clear");
-clear.addEventListener('click', function(){
-    displayValue = "";
-    display.textContent = "0";
-    num1 = num2 = NaN;
-    operator = "";
-})
-
-const addButton = document.querySelector("#add");
-addButton.addEventListener('click', function(){
-    if(operator == ""){
-        num1 = parseInt(displayValue);
+function handleOprClick(e){
+    console.log(e.target.classList[1]);
+    if(operator != "" && e.target.classList[1] != "equals"){
+        if(number1 != null){
+                console.log("Reached here!");
+                operator = carriedOperator;
+                carriedOperator = e.target.classList[1];
+                number2 = parseInt(displayText);
+                number1 = operate(number1, number2, operator);
+                displayText = number1;
+                console.log(number1, number2, displayText, e.target.classList[1]);
+                displayDiv.innerHTML = `<p>${displayText}</p>` 
+                number2 = null;
+                displayText = "";
+                return;
+        
+            }
+        return;
     }
-    else{
-        num1 = parseInt(displayValue);
-        num1 = operate(num1,num2,operator);
-    }
+    if(e.target.classList[1] != "equals"){
+        if(number1 == null){
+            number1 = parseInt(displayText);
+        }else{
+            number2 = parseInt(displayText);
+        }
+        // 
+        operator = e.target.classList[1];
+        carriedOperator = e.target.classList[1];
+        displayText = "";
     
-    console.log(num1);
-    operator = "+";
-    displayValue = "";
-})
-
-const subtractButton = document.querySelector("#subtract");
-subtractButton.addEventListener('click', function(){
-    if(operator == ""){
-        num1 = parseInt(displayValue);
+    }else{
+        if(number1 === null)
+        {
+            return;
+        }
+        number2 = parseInt(displayText);
+        displayText = operate(number1, number2, carriedOperator);
+        displayDiv.innerHTML = `<p>${displayText}</p>` 
+        number1 = parseInt(displayText);
+        number2 = null;
+        operator = "";
+        displayText = "";
     }
-    else{
-        num1 = parseInt(displayValue);
-        num1 = operate(num1,num2,operator);
-    }
-    
-    operator = "-";
-    displayValue = "";
-})
+}
 
-const divideButton = document.querySelector("#divide");
-divideButton.addEventListener('click', function(){
-    if(operator == ""){
-        num1 = parseInt(displayValue);
-    }
-    else{
-        num1 = parseInt(displayValue);
-        num1 = operate(num1,num2,operator);
-    }
-    
-    operator = "/";
-    displayValue = "";
-})
+function handleClear(){
+    number1 = number2 = null;
+    operator = carriedOperator = "";
+    displayText = "";
+    displayDiv.innerHTML = `<p>0</p>` ;
+}
 
-const multiplyButton = document.querySelector("#multiply");
-multiplyButton.addEventListener('click', function(){
-    if(operator == ""){
-        num1 = parseInt(displayValue);
-    }
-    else{
-        num1 = parseInt(displayValue);
-        num1 = operate(num1,num2,operator);
-    }
-    
-    operator = "*";
-    displayValue = "";
-})
+for(const num of numbers){
+    num.addEventListener('click', handleNumClick);
+}
 
-const equals = document.querySelector('#equals');
-equals.addEventListener('click',function(){
-    num2 = parseInt(displayValue);
-    console.log("Num 1: "+num1+" Num2 :"+num2+" Operator: "+operator);
-    displayValue = operate(num1,num2,operator);
-    num1 = parseInt(displayValue);
-    display.textContent = displayValue;
-    displayValue = "";
-    num1 = num2 = NaN;
-    operator = "";
-})
+for(const opr of operators){
+    opr.addEventListener('click', handleOprClick);
+}
 
-
+clear.addEventListener('click', handleClear);
